477bc65b4b58a7f82f229a620ad2f318ed693386
piyush2official <piyush2official@gmail.com>
2016-05-02 08:07:38 +0530

    second commit

--- js/controllers.js
+++ js/controllers.js
@@ -1,24 +1,11 @@
-angular.module('poolin.controllers',[])
+angular.module('poolin.controllers', [])
 
-    .controller("myCtrl", function ($scope , $timeout,$window) {
+    .controller("myCtrl", function ($scope, $timeout, $window) {
 
-        $scope.getposition = function(){
+        $scope.getposition = function () {
             console.log($window.scrollY);
 
         }
-
-
-        setInterval(function(){
-
-            if($window.scrollY > 300){
-                $scope.navcolor = 1;
-            }
-            console.log($window.scrollY);
-
-
-
-        }, 100)
-
 
         $scope.linewidth = 0;
         $scope.hideline = 0;
@@ -42,25 +29,35 @@ angular.module('poolin.controllers',[])
         }, 3000);
 
 
-    $scope.firstDivHide = 0;
-    $scope.secondDivHide = 0;
-    $scope.firstDivShow = 1;
-    $scope.secondDivShow = 1;
+        $scope.firstDivHide = 0;
+        $scope.secondDivHide = 0;
+        $scope.firstDivShow = 1;
+        $scope.secondDivShow = 1;
 
-        $scope.showline =1;
+        $scope.showline = 1;
         $scope.hideline = 0;
 
-    $scope.fadeRight = function () {
-        $scope.secondDivHide = 1;
-        $scope.firstDivShow = 1;
-        $scope.firstDivHide = 0;
-    }
+        $scope.fadeRight = function () {
+            $scope.secondDivHide = 1;
+            $scope.firstDivShow = 1;
+            $scope.firstDivHide = 0;
+        }
 
-    $scope.fadeLeft = function () {
-        $scope.firstDivHide = 1;
-        $scope.secondDivShow = 1;
-        $scope.secondDivHide = 0;
-    }
+        $scope.fadeLeft = function () {
+            $scope.firstDivHide = 1;
+            $scope.secondDivShow = 1;
+            $scope.secondDivHide = 0;
+        }
+
+        setInterval(function () {
+
+            if ($window.scrollY > 300) {
+                $scope.navcolor = 1;
+            }
+            console.log($window.scrollY);
 
 
-});
+        }, 100)
+
+
+    });
